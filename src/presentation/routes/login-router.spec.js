c85d2f2339e6830e5625ec8d5309c31eb6617654
Joisiney Leandro <joisiney@gmail.com>
2022-12-03 18:00:36 -0300

    Fix - Ensure LoginRouter return 400 if no password is providerd

--- src/presentation/routes/login-router.spec.js
+++ src/presentation/routes/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -13,6 +13,18 @@ describe('Login router', () => {
     const httpRequest = {
       body: {
         password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
+describe('Login router', () => {
+  test('Should return 400 if no password is provider', async () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_mail@sitiodigital.com.br'
       }
     }
     const httpResponse = sut.route(httpRequest)
