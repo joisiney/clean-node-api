778f8b76a9927a531d8f5c28d0ac4b3edfcf930d
Joisiney Leandro <joisiney@gmail.com>
2022-12-04 08:44:56 -0300

    Fix - integration AuthUseCase and Encrypter

--- src/domain/use-case/auth-use-case.js
+++ src/domain/use-case/auth-use-case.js
@@ -2,8 +2,9 @@ const { MissingParamError } = require('../../utils/errors')
 
 module.exports = class AuthUseCase {
   accessToken = 'valid_token'
-  constructor (userEmailRepository) {
+  constructor (userEmailRepository, encrypter) {
     this.userEmailRepository = userEmailRepository
+    this.encrypter = encrypter
   }
 
   async auth (email, password) {
@@ -11,6 +12,7 @@ module.exports = class AuthUseCase {
     if (!password) throw new MissingParamError('password')
     const user = await this.userEmailRepository.findByEmail(email)
     if (!user) return null
+    await this.encrypter.compare(password, user.password)
     return null
   }
 }
--- src/domain/use-case/auth-use-case.spec.js
+++ src/domain/use-case/auth-use-case.spec.js
@@ -1,12 +1,17 @@
 const { MissingParamError } = require('../../utils/errors')
 const { AuthUseCase } = require('./')
-const makeAuthUseCase = (userEmailRepository) => {
-  const sut = new AuthUseCase(userEmailRepository)
-  return { sut }
+const makeAuthUseCase = () => {
+  const userEmailRepository = makeFindUserEmailRepositorySpy()
+  const encrypter = makeEncrypter()
+  const sut = new AuthUseCase(userEmailRepository, encrypter)
+  return { sut, userEmailRepository, encrypter }
 }
 const makeFindUserEmailRepositorySpy = () => {
   class FindUserEmailRepositorySpy {
-    user = {}
+    user = {
+      password: 'hashed_password'
+    }
+
     async findByEmail (email) {
       this.email = email
       return this.user
@@ -15,45 +20,60 @@ const makeFindUserEmailRepositorySpy = () => {
   const userEmailRepository = new FindUserEmailRepositorySpy()
   return userEmailRepository
 }
+const makeEncrypter = () => {
+  class EncrypterSpy {
+    async compare (password, hashedPassword) {
+      this.password = password
+      this.hashedPassword = hashedPassword
+    }
+  }
+  const encrypterSpy = new EncrypterSpy()
+  return encrypterSpy
+}
 describe('Auth UseCase', () => {
   test('Should throw null if no email is provided', async () => {
-    const { sut } = makeAuthUseCase(makeFindUserEmailRepositorySpy())
+    const { sut } = makeAuthUseCase()
     sut.accessToken = null
     const ps = sut.auth()
     expect(ps).rejects.toThrow(new MissingParamError('email'))
   })
   test('Should throw null if no password is provided', async () => {
-    const { sut } = makeAuthUseCase(makeFindUserEmailRepositorySpy())
+    const { sut } = makeAuthUseCase()
     sut.accessToken = null
     const ps = sut.auth('email@gmail.com')
     expect(ps).rejects.toThrow(new MissingParamError('password'))
   })
   test('Should call FindUserEmailRepository with correct mail', async () => {
-    const userEmailRepository = makeFindUserEmailRepositorySpy()
-    const { sut } = makeAuthUseCase(userEmailRepository)
+    const { sut, userEmailRepository } = makeAuthUseCase()
     await sut.auth('email@gmail.com', '123')
     expect(userEmailRepository.email).toBe('email@gmail.com')
   })
   test('Should throw if no FindUserEmailRepository is provided', async () => {
     const { sut } = makeAuthUseCase()
+    sut.userEmailRepository = null
     const ps = sut.auth('email@gmail.com', '123')
     expect(ps).rejects.toThrow()
   })
   test('Should return null FindUserEmailRepository return null', async () => {
-    const userEmailRepository = makeFindUserEmailRepositorySpy()
+    const { sut, userEmailRepository } = makeAuthUseCase()
     userEmailRepository.user = null
-    const { sut } = makeAuthUseCase(userEmailRepository)
     const accessToken = await sut.auth('email@gmail.com', 'any_password')
     expect(accessToken).toBeNull()
   })
   test('Should return null if an invalid email is provided', async () => {
-    const { sut } = makeAuthUseCase(makeFindUserEmailRepositorySpy())
-    const accessToken = await sut.auth('email@gmail.com', 'any_password')
+    const { sut } = makeAuthUseCase()
+    const accessToken = await sut.auth('invalid@gmail.com', 'any_password')
     expect(accessToken).toBeNull()
   })
-  // test('Should return null if an invalid password is provided', async () => {
-  //   const { sut } = makeAuthUseCase(makeFindUserEmailRepositorySpy())
-  //   const accessToken = await sut.auth('email@gmail.com', 'invalid_password')
-  //   expect(accessToken).toBeNull()
-  // })
+  test('Should return null if an invalid password is provided', async () => {
+    const { sut } = makeAuthUseCase()
+    const accessToken = await sut.auth('valid@gmail.com', 'invalid_password')
+    expect(accessToken).toBeNull()
+  })
+  test('Should call Encrypter with correct values', async () => {
+    const { sut, encrypter, userEmailRepository } = makeAuthUseCase()
+    await sut.auth('valid@gmail.com', 'hashed_password')
+    expect(encrypter.password).toBe('hashed_password')
+    expect(encrypter.hashedPassword).toBe(userEmailRepository.user.password)
+  })
 })
