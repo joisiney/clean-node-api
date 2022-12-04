76a9d775aab89da4d094ed8351677e3e508158b0
Joisiney Leandro <joisiney@gmail.com>
2022-12-03 22:34:35 -0300

    Fix - Validator email

---
+++ src/utils/validator/email-validator.spec.js
@@ -0,0 +1,19 @@
+const validator = require('validator')
+class EmailValidator {
+  isValid (email) {
+    return validator.isEmail(email)
+  }
+}
+describe('Email validator', () => {
+  test('Should return true if validator returns true', () => {
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('valid@email.com')
+    expect(isEmailValid).toBe(true)
+  })
+  test('Should return false if validator returns false', () => {
+    validator.isEmailValid = false
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('validemail.com')
+    expect(isEmailValid).toBe(false)
+  })
+})
